a627366c9116eaf1d9fa363a694e9c13651249b8
Joge Eduardo Chaves Esquivel <eduardo230898@gmail.com>
2022-07-27 22:55:43 -0600

    update
    cambios en muchos de los directorios

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+window.addEventListener('load', function(){
+    
+    bodyDesing()
+    carouseImg()
+    changeImg()
+}, false)
+
+
+
+function showHover(imagenLogo){
+    if (imagenLogo){
+        imagenLogo.src = "/img/bosque-con-niebla_2560x1440_6701.jpg";
+        console.log('in')
+    }
+}
+function showNormal(imagenLogo){
+    if (imagenLogo){
+        imagenLogo.src = "/img/Forests.jpg";
+        console.log('out')
+    }
+}
+
+function changeImg(){
+    var imagenLogo = document.getElementById('imagen');
+    if (imagenLogo.addEventListener){
+        imagenLogo.addEventListener('mouseover', function(){
+            showHover(imagenLogo);
+        }, false);
+        
+        imagenLogo.addEventListener('mouseout', function(){
+            showNormal(imagenLogo);
+        }, false);
+    }
+
+}
+
+function bodyDesing(){
+    document.body.style.backgroundImage = "url('/img/bosque-con-niebla_2560x1440_6701.jpg')";
+    this.document.body.style.backgroundRepeat = "no-repeat";
+    this.document.body.style.backgroundAttachment = "Fixed";
+    this.document.body.style.position = "relative";
+    this.document.body.style.width = "100%";
+    
+}
+
+function carouseImg(){
+    var firstImg = this.document.getElementById("firstImg");
+    firstImg.src = '/img/bosque-con-niebla_2560x1440_6701.jpg';
+    var secondImg = this.document.getElementById("secondImg");
+    secondImg.src = '/img/Forests.jpg';
+    var thirdImg = this.document.getElementById("thirdImg");
+    thirdImg.src = 'img/gafas-de-sol-para-perros.jpg';
+}
+
+
